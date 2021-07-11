dfd4e8052441dc0e09c79d800037e639b23cba50
Thomas Vuille <thomas@newtomsoft.fr>
2021-07-11 13:54:53 +0200

    version change

--- Newtomsoft.EntityFramework/Core/ServicesExtension.cs
+++ Newtomsoft.EntityFramework/Core/ServicesExtension.cs
@@ -7,8 +7,6 @@ namespace Newtomsoft.EntityFramework.Core
     public static class ServicesExtension
     {
         public static void AddDbContext<T>(this IServiceCollection services, IConfiguration configuration) where T : DbContext
-        {
-            EntityFrameworkTools<T>.AddDbContext(services, configuration);
-        }
+            => EntityFrameworkTools<T>.AddDbContext(services, configuration);
     }
 }
